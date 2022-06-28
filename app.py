fd33de1c5d8c149b3d846c4a8723ee70289e64cb
4killU <kpakhotin@ukr.net>
2022-06-28 14:46:05 +0300

    Added some pages, v0.0.3

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'd1585fd7c253df42cd25b29573c902b65339d71aa342e68b'
+
+messages = [{'title': 'Message One',
+             'content': 'Message One Content'},
+            {'title': 'Message Two',
+             'content': 'Message Two Content'}
+            ]
 
 
 @app.route('/')
 @app.route('/index/')
 def start_app():
-    return render_template('index.html')
+    return render_template('index.html', messages=messages)
 
 
 @app.route('/about/')
@@ -26,4 +33,16 @@ def comments():
 
 @app.route('/create/', methods=('GET', 'POST'))
 def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        content = request.form['content']
+
+        if not title:
+            flash('Title is required!')
+        elif not content:
+            flash('Content is required!')
+        else:
+            messages.append({'title': title, 'content': content})
+            return redirect(url_for('start_app'))
+
     return render_template('create.html')
